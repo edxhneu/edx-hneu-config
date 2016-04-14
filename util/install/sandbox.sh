083b8f3c145fc9ad620dea4af3b74c44f3560c15
edxhneu <edx@hneu.net>
2016-04-14 13:28:30 +0300

    fix pip

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -30,9 +30,14 @@ echo "==============================="
 ##
 echo -e "\nInstall system pre-requisites"
 echo -e "-------------------------------"
-sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
+#sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev
+#sudo pip install --upgrade pip
+#sudo pip install --upgrade virtualenv
+
+sudo apt-get install -y build-essential software-properties-common curl git-core libxml2-dev libxslt1-dev python-pip libmysqlclient-dev python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc-4.8 g++-4.8
+sudo pip install --upgrade pip==7.1.2
+sudo pip install --upgrade setuptools==18.3.2
+sudo -H pip install --upgrade virtualenv==13.1.2
 echo -e "==============================="
 
 ## Did we specify an openedx release?
