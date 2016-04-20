004310464fb54039815458099cb9abe1375ef97d
edxhneu <edx@hneu.net>
2016-04-20 10:16:24 +0300

    for dogwood.2

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -7,7 +7,7 @@
 ## Note that this script requires that you have the ability to run
 ## commands as root via sudo.  Caveat Emptor!
 ##
-
+export OPENEDX_RELEASE=named-release/dogwood.2
 ##
 ## Sanity check
 ##
@@ -15,6 +15,12 @@ if [[ ! "$(lsb_release -d | cut -f2)" =~ $'Ubuntu 12.04' ]]; then
    echo "This script is only known to work on Ubuntu 12.04, exiting...";
    exit;
 fi
+
+##
+## Set ppa repository source for gcc/g++ 4.8 in order to install insights properly
+##
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 
 ##
 ## Update and Upgrade apt packages
@@ -25,9 +31,16 @@ sudo apt-get upgrade -y
 ##
 ## Install system pre-requisites
 ##
-sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
+sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc-4.8 g++-4.8
+sudo pip install --upgrade pip==7.1.2
+sudo pip install --upgrade setuptools==18.3.2
+sudo -H pip install --upgrade virtualenv==13.1.2
+
+##
+## Update alternatives so that gcc/g++ 4.8 is the default compiler
+##
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 ## Did we specify an openedx release?
 if [ -n "$OPENEDX_RELEASE" ]; then
@@ -35,10 +48,14 @@ if [ -n "$OPENEDX_RELEASE" ]; then
     -e certs_version=$OPENEDX_RELEASE \
     -e forum_version=$OPENEDX_RELEASE \
     -e xqueue_version=$OPENEDX_RELEASE \
+    -e configuration_version=$OPENEDX_RELEASE \
+    -e NOTIFIER_VERSION=$OPENEDX_RELEASE \
+    -e INSIGHTS_VERSION=$OPENEDX_RELEASE \
+    -e ANALYTICS_API_VERSION=$OPENEDX_RELEASE \
   "
   CONFIG_VER=$OPENEDX_RELEASE
 else
-  CONFIG_VER="release"
+  CONFIG_VER="master"
 fi
 
 ##
@@ -53,17 +70,9 @@ git checkout $CONFIG_VER
 ## Install the ansible requirements
 ##
 cd /var/tmp/configuration
-sudo pip install -r requirements.txt
-
-##
-## Get configuratuon
-##
-cd /var/tmp/
-wget -O server-vars.yml https://raw.githubusercontent.com/edxhneu/configuration/named-release/cypress-hneu/util/install/server-vars.yml
+sudo -H pip install -r requirements.txt
 
 ##
 ## Run the edx_sandbox.yml playbook in the configuration/playbooks directory
 ##
-cd /var/tmp/configuration/playbooks && sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," -e@/var/tmp/server-vars.yml
-
-cp /var/tmp/server-vars.yml /edx/app/edx_ansible/server-vars.yml
+cd /var/tmp/configuration/playbooks && sudo ansible-playbook -c local ./edx_sandbox.yml -i "localhost," $EXTRA_VARS
